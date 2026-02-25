b0753e4c8fb3bf67186a3fa2f9317f89143102e6
Jared <231g0168@rcarbonifera.tecnm.mx>
2026-02-25 13:57:29 -0600

    Agregar model

---
+++ AplicacionPollos/Models/CajasModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AplicacionPollos.Models
+{
+    public class CajasModel
+    {
+        public int id { get; set; }
+        public int numero_lote { get; set; }
+        public byte rango_peso { get; set; }
+        public decimal peso { get; set; }
+    }
+}
