8c4b4bf80eb4ee5b26249971c8e306b1c641aa18
JuanAgTec <231G0163@rcarbonifera.tecnm.mx>
2026-02-28 18:02:45 -0600

    Cambios realizados: - Primera versión del viewmodel: -Metodos C,U,D (falta leer los datos y que se hagan en el repositorio). -Validación (falta validar el código de barras). -Metodo para cambiar de vistas. -La clase se hizo notificable.
    -Instalación del paquete NuGet MVVM Community Toolkit.
    
    -Se agregaron propiedades opcionales al model CajasModel para ciertos tipos de etiqueta en caso de ser necesarios o útiles para el cliente.

--- AplicacionPollos/Models/CajasModel.cs
+++ AplicacionPollos/Models/CajasModel.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection.Metadata.Ecma335;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -12,5 +13,13 @@ namespace AplicacionPollos.Models
         public int numero_lote { get; set; }
         public byte rango_peso { get; set; }
         public decimal peso { get; set; }
+        public int? numero_id { get; set; } //Son los últimos 4 digitos del ID de algunos tipos de etiqueta
+        public int? numero_empleado { get; set; } //Número del empleado que etiquetó la caja
+        public int? numero_planta { get; set; } //Número de la planta de donde proviene la caja
+        public int? numero_piezas { get; set; } //Número de piezas del producto que incluye la caja
+        public int? id_producto { get; set; } /* Número de ID que utiliza la empresa proveedora para identificar su producto. 
+                                              * NOTA: en el código de barras solo vienen los primeros 4 digitos que identifican al producto.*/
+        public string? proveedor { get; set; } = string.Empty; /* En caso de que luego podamos identificar el proveedor en base al
+                                                              * código de barras (ej. Por el tamaño del código). */
     }
 }
---
+++ AplicacionPollos/ViewModels/CajasViewModel.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using AplicacionPollos.Models;
+using CommunityToolkit.Mvvm.Input;
+
+namespace AplicacionPollos.ViewModels
+{
+    public enum Vistas //Si quieren usar un string, diganme y lo cambio a un string.
+    {
+        Agregar,
+        Editar,
+        Eliminar,
+        Principal,
+        Dialogo //Para mostrar mensajes de error, a no ser que tengan otra idea para mostrarlos.
+    }
+    public class CajasViewModel : INotifyPropertyChanged
+    {
+        public CajasViewModel()
+        {
+            AgregarCommand = new RelayCommand(Agregar);
+            EliminarCommand = new RelayCommand(Eliminar);
+            EditarCommand = new RelayCommand(Editar);
+            CambiarVistaCommand = new RelayCommand<Vistas>(CambiarVista);
+        }
+        public CajasModel? CajaModel { get; set; }
+        public List<string> ListaErrores { get; set; } = new();
+        public Vistas VistaActual { get; set; }
+        public ICommand AgregarCommand { get; set; }
+        public ICommand EliminarCommand { get; set; }
+        public ICommand EditarCommand { get; set; } /* No creo que sea necesario, es imposible que se requiera editar a no ser que exista
+                                                    *  error humano al momento de introducir manualmente el código de barras. (Eliminar de ser necesario) */
+        public ICommand CambiarVistaCommand { get; set; }
+        //TODO: propiedad del repositorio
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        private void ValidarEntrada(CajasModel? cajaModel)
+        {
+            if (cajaModel == null) return;
+
+            if (cajaModel.peso < 0) ListaErrores.Add("El campo 'Peso neto' no puede ser menor o igual que 0. Revise la etiqueta.");
+            if (cajaModel.numero_lote < 0) ListaErrores.Add("El campo 'No. Lote' no fue reconocido, revise el código de barras introducido.");
+
+            //TODO: Validar el código de barras, si no será imposible reconocer los datos. (preferiblemente utilizando RegEx)
+        }
+
+        public void Agregar() 
+        {
+            ValidarEntrada(CajaModel);
+            //TODO: instrucción para agregarlo a la base de datos, recargar datos.
+            CajaModel = null; //Para evitar que se haga referencia a ella justo despues de argegar sin que el usuario la haya seleccionado. Otra opcion es quitarla de Agregar e incluirla en CambiarVista.
+            CambiarVista(Vistas.Principal);
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(CajaModel)));
+        }
+
+        public void Eliminar() 
+        {
+            //TODO: Instrucción para eliminarla de la base de datos (repositorio)
+            CambiarVista(Vistas.Principal);
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(VistaActual)));
+        }
+
+        public void Editar() 
+        {
+            //TODO: Instrucción para editarlo en la base de datos (repositorio)
+            CambiarVista(Vistas.Principal);
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(VistaActual)));
+        }
+
+        private void CambiarVista(Vistas vista)
+        {
+            switch (vista)
+            {
+                case Vistas.Principal:
+                    VistaActual = Vistas.Principal;
+                    PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(VistaActual)));
+                    break;
+                case Vistas.Agregar:
+                    VistaActual = Vistas.Agregar;
+                    CajaModel = new CajasModel();
+
+                    PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(VistaActual)));
+                    PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(CajaModel)));
+                    break;
+                case Vistas.Eliminar:
+                    if (CajaModel == null)
+                    {
+                        ListaErrores.Add("Seleccione una caja para eliminar."); /* Podemos también ignorar esta validación y simplemente no hacer nada cuando no haya nada seleccionado. 
+                                                                                 * Pero nos arriesgamos a que un usuario piense que el programa no funciona al intentar eliminar una entrada nula. */
+                        PropertyChanged.Invoke(this, new PropertyChangedEventArgs(nameof(ListaErrores)));
+                        return;
+                    }
+
+                    VistaActual = Vistas.Eliminar;
+                    PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(VistaActual)));
+                    break;
+                case Vistas.Editar:
+                    if (CajaModel == null)
+                    {
+                        ListaErrores.Add("Seleccione una caja para modificar la información.");
+                        PropertyChanged.Invoke(this, new PropertyChangedEventArgs(nameof(ListaErrores)));
+                        return;
+                    }
+
+                    //TODO: Crear un clon para editarlo en lugar de editar la caja original.
+
+                    VistaActual = Vistas.Editar;
+                    PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(VistaActual)));
+                    break;
+            }
+            //TODO: En caso de seguir usando la vista "Dialogo" modificar este metodo para tomar en cuenta esa vista también.
+
+            ListaErrores.Clear();
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(ListaErrores)));
+        }
+    }
+}
