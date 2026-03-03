d6d64fb1dd40fd8bda4ac8ad434a429170c15e4e
gaelekisde <gael1411hernandez@gmail.com>
2026-03-02 20:20:57 -0600

    Actualizacion del Model:

--- AplicacionPollos/Models/CajasModel.cs
+++ AplicacionPollos/Models/CajasModel.cs
@@ -9,10 +9,12 @@ namespace AplicacionPollos.Models
 {
     public class CajasModel
     {
+        //271254486922289100162628A
+        public string codigo_barras { get; set; } = string.Empty;
         public int id { get; set; }
         public int numero_lote { get; set; }
         public byte rango_peso { get; set; }
-        public decimal peso { get; set; }
+        public decimal peso { get; set; } = 0;
         public int? numero_id { get; set; } //Son los últimos 4 digitos del ID de algunos tipos de etiqueta
         public int? numero_empleado { get; set; } //Número del empleado que etiquetó la caja
         public int? numero_planta { get; set; } //Número de la planta de donde proviene la caja
